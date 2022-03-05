f55f0a8a528e2878dbdc4f0053f563aaf9b5e721
Aman Bakshi <amanbakshi4@gmail.com>
2022-03-05 16:57:38 +0530

    requirements

--- src/core/settings.py
+++ src/core/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
