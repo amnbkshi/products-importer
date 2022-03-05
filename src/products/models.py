4ae5c9fa6192dcd9fd65900f921721df35f0d62a
Aman Bakshi <amanbakshi4@gmail.com>
2022-03-05 19:20:01 +0530

    Model for new app

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    sku = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=1000)
+    status = models.BooleanField(default=False)
