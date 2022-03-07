9e771e1388ce2f07c33c3e82c0f1df0fccf83a40
Aman Bakshi <amanbakshi4@gmail.com>
2022-03-07 16:53:26 +0530

    Serializer for configured webhooks

--- src/products/serializers.py
+++ src/products/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Webhooks
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
+        fields = "__all__"
+
+
+class WebhooksSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Webhooks
         fields = "__all__"
