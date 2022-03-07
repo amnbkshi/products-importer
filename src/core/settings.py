6d63ae55111245cd7a6951677b95e32f1e91d7ad
Aman Bakshi <amanbakshi4@gmail.com>
2022-03-07 14:28:00 +0530

    Added basic authentication to all apis

--- src/core/settings.py
+++ src/core/settings.py
@@ -73,6 +73,13 @@ TEMPLATES = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 20
 }
