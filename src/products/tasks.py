4d7d9f7ef059675ee1c4fe29e79cb513cb4ee579
Aman Bakshi <amanbakshi4@gmail.com>
2022-03-07 13:50:23 +0530

    Integrating celery with channels

--- src/products/tasks.py
+++ src/products/tasks.py
@@ -1,21 +1,29 @@
 import os
 import csv
 from itertools import islice
-from celery import shared_task
+from celery import shared_task, current_task
 from .models import Product
 
 
 @shared_task
 def uploadData(file_path):
+    with open(file_path, "r") as f:
+        reader = csv.reader(f, delimiter = ",", quotechar='"')
+        total_lines = sum(1 for row in reader)
+    
     reader = csv.reader(open(file_path), delimiter=',', quotechar='"')
     header = next(reader)
     batch_size = 10000
+    prog = 0
+
     while True:
         batch = [Product(name=row[0], sku=row[1], description=row[2])
                  for row in islice(reader, batch_size)]
         if not batch:
             break
         Product.objects.bulk_create(batch, batch_size, ignore_conflicts=True)
-    print(batch_size)
+        current_task.update_state(state='PROGRESS', meta={'current': prog, 'total': total_lines})
+        prog += batch_size
     
     os.remove(file_path)
+    return {'current': 100, 'total': 100, }
